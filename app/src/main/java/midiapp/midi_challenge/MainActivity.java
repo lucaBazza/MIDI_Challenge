424ee76fec21c6741779ad3fcec05a1fa620f73a
Paolo <chiste.paolo.securemail@gmail.com>
2016-12-21 08:29:23 +0100

    Importato Jit per inserire la libreria di gestione dei Midi. Consigliata connessione a internet durante la prima compilazione.

--- app/src/main/java/midiapp/midi_challenge/MainActivity.java
+++ app/src/main/java/midiapp/midi_challenge/MainActivity.java
@@ -3,6 +3,8 @@ package midiapp.midi_challenge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pdrogfer.mididroid.MidiFile;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
