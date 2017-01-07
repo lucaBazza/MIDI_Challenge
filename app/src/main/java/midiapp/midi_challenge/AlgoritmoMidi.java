2466fb2f759e9d43367fb46e83097a40701d13f9
Luca Bazzanella <lucabazzanella@MacBook-Pro-di-Luca.local>
2017-01-07 20:47:57 +0100

    aggiunta algoritmo

--- app/src/main/java/midiapp/midi_challenge/AlgoritmoMidi.java
+++ app/src/main/java/midiapp/midi_challenge/AlgoritmoMidi.java
@@ -11,6 +11,9 @@ import java.util.Iterator;
 /**
  * Created by lucabazzanella on 18/12/16.
  * //ogni nota letta aggunge un punteggio da aggiungere a quello totale
+ *
+ * “La musica è una pratica occulta dell’aritmetica, nella quale l’anima non sa di calcolare” (Gottfried Wilhelm von Leibniz)
+ *
  //punteggio = velocita * armonia * timeSignature (
  //velocita = deltaTick^3 //più la velocità è alta più il punteggio aumenta -> Esponenzialmente
  //armonia = sommatoria( nota[]) //quante note sono suonate contemporaneanemnte
@@ -23,6 +26,8 @@ import java.util.Iterator;
 public class AlgoritmoMidi {
     MidiFile mf;
     private static ArrayList <NoteOn> LN = new ArrayList<NoteOn>();  // la lista sarebbe meglio se dinamica in base al delta time
+    private static ArrayList<String> outPut;
+    static int [] tonalita = {1,3,5,6,8,10,11};                      // scala maggiore %12
     static long punteggio = 0;
     long bestCombo =0;
     AlgoritmoMidi(){
@@ -30,14 +35,13 @@ public class AlgoritmoMidi {
     }
     public void loadFile(MidiFile x){
         mf = x;
-
     }
     public ArrayList<String> calc(){
         MidiTrack T = mf.getTracks().get(0);
         Iterator<MidiEvent> it = T.getEvents().iterator();
-        ArrayList<String> outPut = new ArrayList<>();
+        outPut = new ArrayList<>();
 
-        while(it.hasNext()) {
+        while(it.hasNext()) {   //per ogni nota
             MidiEvent E = it.next();
             if(E.getClass().equals(NoteOn.class))   {
                 NoteOn EveNota = (NoteOn)E;
@@ -47,7 +51,7 @@ public class AlgoritmoMidi {
                     long when = EveNota.getTick();
                     long durata = EveNota.getDelta();
                     LN.add(EveNota);
-                    analizzaFraseggio();
+                    aggiornaTonalita();
 
 
 
@@ -71,8 +75,19 @@ public class AlgoritmoMidi {
     private int salto(int notaPartenza, int notaArrivo){
         return notaPartenza - notaArrivo;
     }
-    private void analizzaFraseggio(){
+    private void aggiornaTonalita(){    //confronta l'ultima nota inserita con il vettore ton e dopo il vettore last, e decide se una nota è diatonica, non, o se è avvenuto un cambio ton
+        NoteOn lastNota = LN.get(LN.size());
+        boolean diatonica = true;
 
+        for(int i=0;i<7;i++)    //controllo se diatonica
+            if(lastNota.getNoteValue()%12 == tonalita[i]) diatonica = false;
+
+        if(!diatonica)          //Se NON è diatonica procedo con un controllo per vedere se è necessario un cambio tonalità
+            for(int i=LN.size()-1; i>0; i--){ //for dall'ultimo al primo
+                if(lastNota.getNoteValue()%12 == LN.get(i).getNoteValue()%12) { //se è la stessa nota vuol dire che la nota diatonica è stata ripetuta, quindi la ton va aggiornata
+                    outPut.add("Cambio tonalità a: "+lastNota.getDelta()/1000);
+                }
+            }
     }
 
 }
