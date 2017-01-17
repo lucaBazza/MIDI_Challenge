a90556fb030b207a0d16cdb03609c6bdbc4d1f6c
Paolo Chistè <paolo.chiste-2@studenti.unitn.it>
2017-01-17 17:40:34 +0100

    Aggiunta activity pagina principale

---
+++ app/src/main/java/midiapp/midi_challenge/PaginaPrincipale_Activity.java
@@ -0,0 +1,13 @@
+package midiapp.midi_challenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaginaPrincipale_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_principale);
+    }
+}
