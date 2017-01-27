65b58de5848cb0b94a766567d479d970ede64f21
namron12 <norman.gabalin@gmail.com>
2017-01-27 11:29:13 +0100

    RI - creazione activity INFO

--- app/src/main/java/midiapp/midi_challenge/Impostazioni_Activity.java
+++ app/src/main/java/midiapp/midi_challenge/Impostazioni_Activity.java
@@ -1,10 +1,10 @@
 package midiapp.midi_challenge;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+
 
 public class Impostazioni_Activity extends AppCompatActivity {
 
@@ -14,12 +14,14 @@ public class Impostazioni_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_impostazioni_);
         this.setTitle("Impostazioni");
 
-        ActionBar ac = this.getSupportActionBar();
+        android.support.v7.app.ActionBar ac = this.getSupportActionBar();
         ac.setDisplayHomeAsUpEnabled(true);
+
     }
 
     public void openNewActivity(View view) {
-        //Intent startActivity = new Intent(this, Activity_info.class);
-        //startActivity(startActivity);
+        Intent startActivity = new Intent(this, Info_Activity.class);
+        startActivity(startActivity);
     }
+
 }
