e79f6ad996fa92844b9563fb2382b9731b208547
namron12 <norman.gabalin@gmail.com>
2017-05-18 14:57:51 +0200

    Inserimento sfondo applicazione

--- app/src/main/java/midiapp/midi_challenge/MetronomoActivity.java
+++ app/src/main/java/midiapp/midi_challenge/MetronomoActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.media.MediaPlayer;
+import android.icu.text.DateFormat;
+import java.sql.Date;
+import android.icu.text.SimpleDateFormat;
+
 
 public class MetronomoActivity extends GenericMIDIChallengeActivity {
     int cont = 120;
@@ -31,15 +38,32 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
                 R.id.counter);
         displayInteger.setText("" + number);
     }
+
+    /**
+    start_button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            music = MediaPlayer.create(MainActivity.this, R.raw.beep); //aggiunto
+
+            Timer timer = new Timer("MetronomeTimer", true);
+            TimerTask tone = new TimerTask() {
+                @Override
+                public void run() {
+                    //Log
+                    DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss:SS");
+                    Date date = new Date();
+                    j++;
+                    Log.i("Beep", df.format(date) + "_____" + j);
+                    //Play sound
+                    music.start();
+                }
+            };
+            timer.scheduleAtFixedRate(tone, 500, 500); // ogni 500 ms
+        }
+    });
+     */
+
 }
-
-
-
-
-
-
-
-
 
 
 
