5cf7c26d8ea56fbe89f7605d2e36ac70fa289a48
namron12 <norman.gabalin@gmail.com>
2017-05-18 18:18:34 +0200

    Modifiche minori

--- app/src/main/java/midiapp/midi_challenge/MetronomoActivity.java
+++ app/src/main/java/midiapp/midi_challenge/MetronomoActivity.java
@@ -1,16 +1,20 @@
 package midiapp.midi_challenge;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import android.media.MediaPlayer;
-import android.icu.text.DateFormat;
-import java.sql.Date;
-import android.icu.text.SimpleDateFormat;
-
 
 public class MetronomoActivity extends GenericMIDIChallengeActivity {
     int cont = 120;
@@ -39,11 +43,19 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
         displayInteger.setText("" + number);
     }
 
+
     /**
+
+     //da finire creazione suono
+
+    ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 50);
+    toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200); //200ms
+
+    Button start_button = (Button) findViewById(R.id.playButton);
+
     start_button.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            music = MediaPlayer.create(MainActivity.this, R.raw.beep); //aggiunto
 
             Timer timer = new Timer("MetronomeTimer", true);
             TimerTask tone = new TimerTask() {
@@ -52,16 +64,18 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
                     //Log
                     DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss:SS");
                     Date date = new Date();
-                    j++;
-                    Log.i("Beep", df.format(date) + "_____" + j);
+                    Log.i("Beep", df.format(date) + "_____");
                     //Play sound
+                    music = MediaPlayer.create(MetronomoActivity.this, R.raw.beep);
                     music.start();
                 }
             };
-            timer.scheduleAtFixedRate(tone, 500, 500); // ogni 500 ms
+            timer.scheduleAtFixedRate(tone, 500, 500); // ogni 500ms
         }
     });
-     */
+
+*/
+
 
 }
 
