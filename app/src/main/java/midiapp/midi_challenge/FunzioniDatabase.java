c0f1f3125aec4c64532d7dc909f14cf65b0b9404
Paolo <chiste.paolo.securemail@gmail.com>
2017-05-27 15:41:26 +0200

    Corretta visualizzazione brani
    Ora i brani mostrati dell'utente sono effettivamente i brani aggiunti
    dall'utente.

--- app/src/main/java/midiapp/midi_challenge/FunzioniDatabase.java
+++ app/src/main/java/midiapp/midi_challenge/FunzioniDatabase.java
@@ -145,7 +145,7 @@ public class FunzioniDatabase {
 
     public List<Brano> braniUtente(long idUtente){
         List<Brano> tmpList = new ArrayList<>();
-        String selectionQuery = "SELECT DISTINCT Brano.idBrano,titolo,nomeFile,difficoltà,autovalutazione,autore,arraySpartiti FROM Brano JOIN relUtenteBrano WHERE idUtente = ?";
+        String selectionQuery = "SELECT DISTINCT Brano.idBrano,titolo,nomeFile,difficoltà,autovalutazione,autore,arraySpartiti FROM Brano JOIN relUtenteBrano ON Brano.idBrano = relUtenteBrano.idBrano WHERE relUtenteBrano.idUtente = ?";
 
         Cursor res = database.rawQuery(selectionQuery,new String[]{Long.toString(idUtente)});
         while(res.moveToNext()){
