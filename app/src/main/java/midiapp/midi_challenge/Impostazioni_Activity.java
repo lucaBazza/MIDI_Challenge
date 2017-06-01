45a60eaa58169cb55c2bf8ae95fbab5a978c978f
namron12 <norman.gabalin@gmail.com>
2017-06-01 12:18:30 +0200

    Aggiunta Import/Export/Delete dati

--- app/src/main/java/midiapp/midi_challenge/Impostazioni_Activity.java
+++ app/src/main/java/midiapp/midi_challenge/Impostazioni_Activity.java
@@ -6,9 +6,22 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import android.os.Environment;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 
 
-public class Impostazioni_Activity extends AppCompatActivity {
+public class Impostazioni_Activity extends AppCompatActivity implements OnClickListener {
+
+    private static final String SAMPLE_DB_NAME = "TrekBook";
+    private static final String SAMPLE_TABLE_NAME = "Info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,23 @@ public class Impostazioni_Activity extends AppCompatActivity {
         android.support.v7.app.ActionBar ac = this.getSupportActionBar();
         ac.setDisplayHomeAsUpEnabled(true);
 
+        findViewById(R.id.btn_import).setOnClickListener(this);
+        findViewById(R.id.btn_export).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_import:
+                createDB();
+                break;
+            case R.id.btn_export:
+                exportDB();
+                break;
+            case R.id.btn_delete:
+                deleteDB();
+                break;
+        }
     }
 
     public void openNewActivity(View view) {
@@ -36,6 +66,49 @@ public class Impostazioni_Activity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+
+    private void deleteDB(){
+        boolean result = this.deleteDatabase(SAMPLE_DB_NAME);
+        if (result==true) {
+            Toast.makeText(this, "Dati cancellati!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void createDB() {
+        SQLiteDatabase sampleDB =  this.openOrCreateDatabase(SAMPLE_DB_NAME, MODE_PRIVATE, null);
+        sampleDB.execSQL("CREATE TABLE IF NOT EXISTS " +
+                SAMPLE_TABLE_NAME +
+                " (LastName VARCHAR, FirstName VARCHAR," +
+                " Rank VARCHAR);");
+        sampleDB.execSQL("INSERT INTO " +
+                SAMPLE_TABLE_NAME +
+                " Values ('Kirk','James, T','Captain');");
+        sampleDB.close();
+        sampleDB.getPath();
+        Toast.makeText(this, "Dati Importati @ "+sampleDB.getPath(), Toast.LENGTH_LONG).show();
+    }
+
+    private void exportDB(){
+        File sd = Environment.getExternalStorageDirectory();
+        File data = Environment.getDataDirectory();
+        FileChannel source=null;
+        FileChannel destination=null;
+        String currentDBPath = "/data/"+ "com.authorwjf.sqliteexport" +"/databases/"+SAMPLE_DB_NAME;
+        String backupDBPath = SAMPLE_DB_NAME;
+        File currentDB = new File(data, currentDBPath);
+        File backupDB = new File(sd, backupDBPath);
+        try {
+            source = new FileInputStream(currentDB).getChannel();
+            destination = new FileOutputStream(backupDB).getChannel();
+            destination.transferFrom(source, 0, source.size());
+            source.close();
+            destination.close();
+            Toast.makeText(this, "Dati esportati!", Toast.LENGTH_LONG).show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
