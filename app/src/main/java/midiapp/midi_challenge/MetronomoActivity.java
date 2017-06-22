42f5a65cca73ed6e334c0ea60bba9576ebd0b3b7
namron12 <norman.gabalin@gmail.com>
2017-06-22 15:45:03 +0200

    Correzione metronomo (DA FINIRE)

--- app/src/main/java/midiapp/midi_challenge/MetronomoActivity.java
+++ app/src/main/java/midiapp/midi_challenge/MetronomoActivity.java
@@ -11,9 +11,79 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+public class MetronomoActivity extends GenericMIDIChallengeActivity {
+
+    private int sound = R.raw.click01;
+    private int bpm = 120;
+    Timer mainTimer;
+    Timer subTimer;
+    MyTimerTask mainTimerTask;
+    MyTimerTask subTimerTask;
+    int swing = ((60 * 1000)/bpm)/12;
+    Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metronomo);
+
+        /*tv_counterBPM = (TextView) findViewById(R.id.counterBPM);
+        tv_metronomoLog = (TextView) findViewById(R.id.tv_metronomoLog);
+        bpm = Integer.parseInt(tv_counterBPM.getText().toString()); */
+
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                play();
+            }
+        });
+    }
+
+
+    public void play() {
+        mainTimer = new Timer("MetronomeTimer", true);
+        subTimer = new Timer();
+        mainTimerTask = new MyTimerTask();
+        subTimerTask = new MyTimerTask();
+
+        mainTimer.schedule(mainTimerTask, 0, 600000 / bpm);
+        subTimer.schedule(subTimerTask, (300 * (100 + swing)) / bpm, 600000 / bpm);
+
+    }
+
+
+    private void playSound() {
+        final MediaPlayer mplayer = MediaPlayer.create(this, sound);
+        if(mplayer==null)
+            Snackbar.make(getWindow().getDecorView().getRootView(), "mplayer null", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        try {
+            mplayer.prepare();
+            mplayer.start();
+            mplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
+        catch (IOException ioex){ ioex.printStackTrace();}
+    }
+
+    class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            playSound();
+        }
+    }
+}
+
+
+/*
 
 public class MetronomoActivity extends GenericMIDIChallengeActivity {
     private int sound = R.raw.a440hz05sec;
@@ -51,13 +121,13 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
         if (bpm < 300) {
             bpm = bpm + 4;
             display(bpm);
-        } else {}
+        }
     }
     public void decreaseInteger(View view) {
         if(bpm > 0) {
             bpm = bpm - 4;
             display(bpm);
-        } else {}
+        }
     }
 
     private void display(int number) {
@@ -68,13 +138,11 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
     private  void startMetronomo(){
 
         final int periodoTimer = 500;
-
-        /*
         Timer timer = new Timer("MetronomeTimer", true);
-        timer.scheduleAtFixedRate(tone, 600000/bpm, periodoTimer);
+
         TimerTask tone = new TimerTask() {
             @Override
-            public void run() {  //riproduzione
+            public void run() {
                 try{
                     mplayer.prepare();
                     mplayer.start();
@@ -83,9 +151,9 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
                     Snackbar.make(getWindow().getDecorView().getRootView(), ex.toString() , Snackbar.LENGTH_SHORT).setAction("Action", null).show();
                 }
             }
-        }; */
+        };
 
-
+        timer.scheduleAtFixedRate(tone, 600000/bpm, periodoTimer);
 
         new java.util.Timer().schedule(new TimerTask(){
             int contaTempoPassato = 0;
@@ -94,9 +162,7 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
                 try{
                     contaTempoPassato += periodoTimer;
                     tv_metronomoLog.setText("tempo passato: "+contaTempoPassato);
-                    /*
-                    mplayer.prepare();
-                    mplayer.start();*/
+                    mplayer.start();
                 }
                 catch(Exception ex){
                     Snackbar.make(getWindow().getDecorView().getRootView(), ex.toString() , Snackbar.LENGTH_SHORT).setAction("Action", null).show();
@@ -111,3 +177,4 @@ public class MetronomoActivity extends GenericMIDIChallengeActivity {
         return true;
     }
 }
+*/
